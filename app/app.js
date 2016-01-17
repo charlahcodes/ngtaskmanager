fd8bd31b654eaae460542fc083c8c26cccb0a1d5
Charlie Miller <charlie@charlah.com>
2016-01-17 17:10:35 -0500

    can add, edit, delete

--- app/app.js
+++ app/app.js
@@ -10,7 +10,9 @@ angular.module('testApp').controller('TaskListCtrl', function($scope){
 
   $scope.taskObj = {
     name: '',
-    description: ''
+    description: '',
+    date: '',
+    index: ''
   };
 
   var Task = function (obj) {
@@ -19,39 +21,54 @@ angular.module('testApp').controller('TaskListCtrl', function($scope){
     this.date = new Date();
   };
 
-// $scope.name isn't defined
+
   var RevTask = function (obj) {
     this.name = $scope.taskObj.name;
     this.desc = $scope.taskObj.description;
+    this.date = $scope.taskObj.date;
+    this.index = $scope.taskObj.date;
   };
 
   $scope.addTask = function (task) {
     let x = new Task(task);
     $scope.taskList.push(x);
-    console.log($scope.taskList);
+    $scope.taskObj.index = ($scope.taskList.length - 1);
 
   };
 
 // When existing task is clicked
   $scope.edit = function (task, index) {
+    $scope.taskObj = {
+      name: $scope.taskList[index].name,
+      description: $scope.taskList[index].desc
+    };
+    console.log($scope.taskList[index]);
+    console.log($scope.taskObj);
+
+
     $scope.taskNameInput = task.taskName;
     $scope.taskDesc = task.taskDesc;
     $scope.index = index;
-    console.log($scope.taskList);
 
 
-    $scope.taskObj = {
-      name: task.taskName,
-      description: task.taskDesc
-    };
+    // $scope.taskObj = {
+    //   name: task.taskName,
+    //   description: task.taskDesc,
+    //   date: new Date()
+    // };
   };
 
   $scope.editSubmit = function(task) {
     let x = new RevTask(task);
-    $scope.taskList[$scope.index] = x;
-    console.log($scope.taskObj);
-    console.log($scope.index);
+    $scope.taskList[$scope.taskObj.index] = x;
+    console.log($scope.taskList);
+  };
 
+  $scope.deleteTask = function(task) {
+    console.log($scope.taskList[0]);
+    $scope.taskList.splice($scope.taskObj.index,1);
+    console.log($scope.taskList);
+    
   };
 	
 
