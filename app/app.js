9d146305a81356c92a8e74d40770bc047769ec8b
Charlie Miller <charlie@charlah.com>
2016-01-17 16:02:27 -0500

    Can add and edit items

--- app/app.js
+++ app/app.js
@@ -2,7 +2,66 @@
 
 angular.module('testApp', []);
 
-angular.module('testApp').controller('TaskListCtrl', function(){
-	var taskList = this;
+angular.module('testApp').controller('TaskListCtrl', function($scope){
+
+  $scope.taskList = [];
+
+  $scope.index = '';
+
+  $scope.taskObj = {
+    name: '',
+    description: ''
+  };
+
+  var Task = function (obj) {
+    this.name = obj.taskName;
+    this.desc = obj.taskDesc;
+    this.date = new Date();
+  };
+
+// $scope.name isn't defined
+  var RevTask = function (obj) {
+    this.name = $scope.taskObj.name;
+    this.desc = $scope.taskObj.description;
+  };
+
+  $scope.addTask = function (task) {
+    let x = new Task(task);
+    $scope.taskList.push(x);
+    console.log($scope.taskList);
+
+  };
+
+// When existing task is clicked
+  $scope.edit = function (task, index) {
+    $scope.taskNameInput = task.taskName;
+    $scope.taskDesc = task.taskDesc;
+    $scope.index = index;
+    console.log($scope.taskList);
+
+
+    $scope.taskObj = {
+      name: task.taskName,
+      description: task.taskDesc
+    };
+  };
+
+  $scope.editSubmit = function(task) {
+    let x = new RevTask(task);
+    $scope.taskList[$scope.index] = x;
+    console.log($scope.taskObj);
+    console.log($scope.index);
+
+  };
+	
+
+
+
+
+  var taskList = this;
 	return taskList;
+
+
+
+
 });
